dbb94b4dfd0a480d18bd50e451b9a3caf3037a2c
Jon Canning <jon.canning@gmail.com>
2015-01-05 21:48:36 +0000

    initial commit

---
+++ Extensions.fs
@@ -0,0 +1,13 @@
+﻿module Raven.Client
+
+open Raven.Client
+
+let openSession (documentStore : IDocumentStore) = documentStore.OpenSession()
+
+let load<'a when 'a : null> (id : string) (documentSession : IDocumentSession) =
+  match documentSession.Load<'a> id with
+  | null -> None
+  | o -> Some o
+
+let store o (documentSession : IDocumentSession) = documentSession.Store o
+let saveChanges (documentSession : IDocumentSession) = documentSession.SaveChanges()
