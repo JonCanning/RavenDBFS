6cc9e2b5edb412fd3a6f0ca6cc32612cdd6991aa
Jon Canning <jon.canning@gmail.com>
2015-01-06 08:09:08 +0000

    box load so records can be used

--- Extensions.fs
+++ Extensions.fs
@@ -4,8 +4,8 @@ open Raven.Client
 
 let openSession (documentStore : IDocumentStore) = documentStore.OpenSession()
 
-let load<'a when 'a : null> (id : string) (documentSession : IDocumentSession) =
-  match documentSession.Load<'a> id with
+let load<'a> (id : string) (documentSession : IDocumentSession) =
+  match documentSession.Load<'a> id |> box with
   | null -> None
   | o -> Some o
 
