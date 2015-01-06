8ac2f1c170f02c583c217c05205f1fde1b96cb01
Jon Canning <jon.canning@gmail.com>
2015-01-06 08:27:13 +0000

    expiry date and unbox load

--- Extensions.fs
+++ Extensions.fs
@@ -1,13 +1,17 @@
 ﻿module Raven.Client
 
 open Raven.Client
+open System
+open Raven.Json.Linq
 
 let openSession (documentStore : IDocumentStore) = documentStore.OpenSession()
 
-let load<'a> (id : string) (documentSession : IDocumentSession) =
+let load<'a> (id : string) (documentSession : IDocumentSession) = 
   match documentSession.Load<'a> id |> box with
   | null -> None
-  | o -> Some o
+  | o -> unbox<'a> o |> Some
 
 let store o (documentSession : IDocumentSession) = documentSession.Store o
 let saveChanges (documentSession : IDocumentSession) = documentSession.SaveChanges()
+let setExpiration o (dateTime : DateTime) (documentSession : IDocumentSession) = 
+  documentSession.Advanced.GetMetadataFor(o).["Raven-Expiration-Date"] <- RavenJValue(dateTime)
