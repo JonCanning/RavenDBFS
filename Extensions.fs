15f415bf8ce47c9d81a9c630e72940f946dcca36
Jon Canning <jon.canning@gmail.com>
2015-01-08 14:14:07 +0000

    Update Extensions.fs

--- Extensions.fs
+++ Extensions.fs
@@ -1,10 +1,13 @@
-﻿module Raven.Client
+module Raven.Client
 
 open Raven.Client
+open Raven.Client.Document
 open System
 open Raven.Json.Linq
 
 let openSession (documentStore : IDocumentStore) = documentStore.OpenSession()
+
+let createDocumentStore connectionStringName = (new DocumentStore(ConnectionStringName = connectionStringName)).Initialize()
 
 let load<'a> (id : string) (documentSession : IDocumentSession) = 
   match documentSession.Load<'a> id |> box with
