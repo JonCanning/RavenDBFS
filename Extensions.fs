e4057cfe3abdaa06408932debdfe246a347a083f
Jon Canning <jon.canning@gmail.com>
2015-01-13 13:50:40 +0000

    Update Extensions.fs

--- Extensions.fs
+++ Extensions.fs
@@ -10,14 +10,9 @@ let createDocumentStore connectionStringName =
   (new DocumentStore(ConnectionStringName = connectionStringName)).Initialize()
 
 let load<'a> (id : string) (documentSession : IDocumentSession) = 
-  match documentSession.Load<'a> id |> box with
-  | null -> None
-  | o -> 
-    try 
-      unbox<'a> o |> Some
-    with
-    | :? InvalidCastException -> None
-    | ex -> raise ex
+  match documentSession.Load<'a> id with
+  | o when box o = null -> None
+  | o -> Some o
 
 let store o (documentSession : IDocumentSession) = documentSession.Store o
 let saveChanges (documentSession : IDocumentSession) = documentSession.SaveChanges()
