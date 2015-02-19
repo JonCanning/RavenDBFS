061af0f16a4a6bd4d2ed48c44deb305d8f481262
Jon Canning <jon.canning@gmail.com>
2015-02-19 17:24:31 +0000

    Update Extensions.fs

--- Extensions.fs
+++ Extensions.fs
@@ -77,3 +77,5 @@ let getAllFromQuery documentStore (query : IDocumentSession -> IDocumentQuery<'a
       | _ -> yield! load count
     }
   load 0
+
+let bulkInsert (documentStore : IDocumentStore) = documentStore.BulkInsert()
